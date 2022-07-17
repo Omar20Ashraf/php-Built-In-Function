3a63a950f6321934c2d40c5fd5a4e12d1e8ba183
Omar20Ashraf <omaarashraf2020@gmail.com>
2022-07-17 14:33:58 +0200

    first Built in function -- str_repeat

---
+++ String/str_repeat.php
@@ -0,0 +1,29 @@
+<?php 
+
+
+// str_repeat();
+
+/* 
+    Function Accept 3 Parameters
+    $string = String to repeat [Mandatory]
+    $repeats = Repeats Count  [Optional], Default is 2
+    $separator = separator between repeats  [Optional], Default is false
+
+*/
+
+function string_repeat(string $str, int $repeats =2, bool $separator=false)
+{
+    # code...
+    $result = '';
+
+    $separator = $separator ? ' ' : '';
+
+    for($i=1; $i<=$repeats; $i++){
+
+        $result.= $str . $separator;
+    }
+
+    return $result;
+}
+
+echo string_repeat('Repeat',3,' ');
